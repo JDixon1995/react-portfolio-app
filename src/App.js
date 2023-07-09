6c19fcf995f561793be3b544d4d50468dec49f98
John <jddhalogamer@gmail.com>
2023-07-09 16:37:15 -0500

    built Contact component, and added context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
+import { useContext } from 'react';
+import { ThemeContext } from './context';
 import Intro from './components/intro/Intro'
 import About from './components/about/About'
 import ProductList from './components/productList/productList'
+import Contact from './components/contact/Contact'
 
 function App() {
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode
   return (
-    <div className="App">
+    <div 
+    className="App"
+    style={{
+      backgroundColor: darkMode ? "#222" : "white",
+      color: darkMode && "white"
+    }}
+    >
       <Intro />
       <About />
       <ProductList />
+      <Contact />
     </div>
   );
 }
